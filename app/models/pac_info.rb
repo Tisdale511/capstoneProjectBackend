ff458a80ac2b303c1de006285d83a4ccedd18c52
Tisdale511 <tisdalefry@gmail.com>
2020-10-15 17:00:42 -0700

    added CommitteeContribution functionality that returns an array of hashes, where each has the key of committee ID, and the key is the total transaction amount given to a candidate

--- app/models/pac_info.rb
+++ app/models/pac_info.rb
@@ -1,4 +1,9 @@
 class PacInfo < ApplicationRecord
     has_many :committee_contributions
     has_many :candidate_infos, through: committee_contributions
+
+    def parsed_pac_info
+        parsed_pac_info = PacInfo.select(:id, :committee_id, :committee_name, :committee_type).where(committee_type: ["Q", "N"])
+        render json: parsed_pac_info
+    end
 end
